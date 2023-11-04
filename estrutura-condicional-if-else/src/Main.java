d6c7df8f5e1e09fc98af37fd5d7d1504c24f013d
Eryck Barreto <eryck.barreto@hotmail.com>
2023-11-03 21:27:33 -0300

    estrutura condicional composta

--- estrutura-condicional-if-else/src/Main.java
+++ estrutura-condicional-if-else/src/Main.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int x = 5;
 
-        System.out.println("Bom dia");
+        Scanner sc = new Scanner(System.in);
+        int hora;
 
-        if (x < 0) {
+        System.out.println("Quantas horas?");
+        hora = sc.nextInt();
+
+        if (hora < 12) {
+            System.out.println("Bom dia");
+        } else {
             System.out.println("Boa tarde");
         }
-
-        System.out.println("Bom noite");
+        sc.close();
 
     }
 }
