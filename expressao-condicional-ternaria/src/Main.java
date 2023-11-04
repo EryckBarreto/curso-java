43f304883d4cccc9847aa2a1423b96b6b2965046
Eryck Barreto <eryck.barreto@hotmail.com>
2023-11-04 13:55:44 -0300

    expressão condicional ternária

---
+++ expressao-condicional-ternaria/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+//        if (preco < 20.0) {
+//            desconto = preco * 0.1;
+//        } else {
+//            desconto = preco * 0.05;
+//        }
+
+        System.out.println(desconto);
+
+    }
+}
