ae9fd11d9613066fb6e964e64fb0c179d36e9c7f
Eryck Barreto <eryck.barreto@hotmail.com>
2023-11-03 21:22:57 -0300

    estrutura de código condicional simples

---
+++ estrutura-condicional-if-else/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        System.out.println("Bom dia");
+
+        if (x < 0) {
+            System.out.println("Boa tarde");
+        }
+
+        System.out.println("Bom noite");
+
+    }
+}
