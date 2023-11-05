be8132f946c212978fe44bec3b0d9c7e6336d1fd
Eryck Barreto <eryck.barreto@hotmail.com>
2023-11-05 13:01:32 -0300

    finalizado o algoritmo com adição e removação de produtos

--- introducao-poo-questao-problema/src/application/Program.java
+++ introducao-poo-questao-problema/src/application/Program.java
@@ -22,7 +22,24 @@ public class Program {
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
 
-        System.out.println(product);
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
 
         sc.close();
     }
