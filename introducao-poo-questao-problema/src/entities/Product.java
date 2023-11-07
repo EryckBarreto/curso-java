ea9ac3a60bad4791b3862f0b8a1d6a83a2b1a419
Eryck Barreto <eryck.barreto@hotmail.com>
2023-11-07 20:48:03 -0300

    refatoração do exercício usando encapsulamento

--- introducao-poo-questao-problema/src/entities/Product.java
+++ introducao-poo-questao-problema/src/entities/Product.java
@@ -2,14 +2,42 @@ package entities;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product() { //construtor padrão
+    }
 
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public double totalValueInStock() {
