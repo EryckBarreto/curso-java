591015d4c83f05d7b889d4f29e53e825dcf716e6
Eryck Barreto <eryck.barreto@hotmail.com>
2023-11-11 10:50:27 -0300

    uso da IDE para gerar automaticamente o constructor, get e set

--- introducao-poo-questao-problema/src/entities/Product.java
+++ introducao-poo-questao-problema/src/entities/Product.java
@@ -6,7 +6,7 @@ public class Product {
     private double price;
     private int quantity;
 
-    public Product() { //construtor padrão
+    public Product() {
     }
 
     public Product(String name, double price, int quantity) {
@@ -20,16 +20,16 @@ public class Product {
         this.price = price;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return this.name;
-    }
-
     public double getPrice() {
-        return this.price;
+        return price;
     }
 
     public void setPrice(double price) {
@@ -39,6 +39,7 @@ public class Product {
     public int getQuantity() {
         return quantity;
     }
+
 
     public double totalValueInStock() {
         return price * quantity;
